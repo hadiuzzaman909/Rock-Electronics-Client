d98bd20e57f4f3a9e70e0b544c27452007f26b28
wakeupzaman909 <hadiuzzaman909@outlook.com>
2022-05-07 22:06:23 +0600

    Add Items added

---
+++ src/Pages/AddNew/AddNew.js
@@ -0,0 +1,45 @@
+import React from 'react';
+import './AddNew.css'
+import { useForm } from "react-hook-form";
+const AddNew = () => {
+    const { register, handleSubmit } = useForm();
+
+
+    const onSubmit = data => {
+        const proceed = window.confirm('Are you sure?');
+        console.log(data);
+        if (proceed) {
+            const url = 'http://localhost:5000/items';
+            fetch(url, {
+                method: 'POST',
+                headers: {
+                    'content-type': 'application/json'
+                },
+                body: JSON.stringify(data)
+            })
+                .then(res => res.json())
+                .then(result => { })
+
+        }
+
+    }
+    return (
+        <div className='mx-auto w-50'>
+            <h2 className='text-center'>Add New Items</h2>
+            <div className='addNew'>
+                <form className='d-flex flex-column' onSubmit={handleSubmit(onSubmit)}>
+                    <input className='mb-2 inputField' placeholder=' Name' {...register("name", { required: true, maxLength: 20 })} />
+                    <textarea className='mb-2 inputField-des'placeholder='Description'{...register("description")} />
+                    <input className='mb-2 inputField'placeholder=' Price' type="number" {...register("price")} />
+                    <input className='mb-2 inputField'placeholder=' Quantity' type="number" {...register("quantity")} />
+                    <input className='mb-2 inputField'placeholder=' Supplier' {...register("Supplier_name")} />
+                    <input className='mb-2 inputField'placeholder=' Photo URL' type='text' {...register("img")} />
+                    <input className='submit-addItem mx-auto mt-3' type="submit" value='Add Item' />
+                </form>
+
+            </div>
+
+        </div>
+    );
+};
+export default AddNew;
